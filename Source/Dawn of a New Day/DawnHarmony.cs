2faf96c177d7f0475dcf265fdd071b001b0d45b7
AlenDio0 <alendio.0000@gmail.com>
2026-07-30 02:14:59 +0200

    Added Harmony 2.4.2

---
+++ Source/Dawn of a New Day/DawnHarmony.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace DawnNewDay
+{
+    [StaticConstructorOnStartup]
+    public static class DawnHarmony
+    {
+        static DawnHarmony()
+        {
+            Harmony harmony = new Harmony("com.alendio.dawnofanewday");
+            harmony.PatchAll();
+        }
+    }
+}
