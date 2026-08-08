2a33501e7eff14bbadd3bd91ae05339de722d855
AlenDio0 <127024738+AlenDio0@users.noreply.github.com>
2026-08-08 22:37:13 +0200

    Fixed compatibility for RimWorld 1.5
    Added a patch for RimWorld 1.5

---
+++ Source/Dawn of a New Day/RimWorld15Patches.cs
@@ -0,0 +1,27 @@
+﻿#if RW_15
+
+using UnityEngine;
+
+namespace DawnNewDay
+{
+    public static class RimWorld15Patches
+    {
+        public static Color WithAlpha(this Color color, float alpha)
+        {
+            color.a = alpha;
+            return color;
+        }
+
+        public static Rect MiddlePart(this Rect rect, float widthPercentage, float heightPercentage)
+        {
+            float width = rect.width * widthPercentage;
+            float height = rect.height * heightPercentage;
+            float x = rect.x + (rect.width - width) / 2f;
+            float y = rect.y + (rect.height - height) / 2f;
+
+            return new Rect(x, y, width, height);
+        }
+    }
+}
+
+#endif
